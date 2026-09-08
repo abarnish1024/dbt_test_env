bc6df5f0d87801beee366ed4fb8aedbade493b60
abarnish1024 <abarnish@omni.co>
2026-09-08 14:02:05 -0400

    swap names

--- models/marts/fct_race_results.sql
+++ models/marts/fct_race_results.sql
@@ -9,7 +9,7 @@ select
   drivers.nationality as driver_nationality,
   results.grid,
   results.finish_position,
-  results.points,
+  results.race_points,
   results.laps
 from {{ ref('stg_results') }} as results
 left join {{ ref('stg_races') }} as races
